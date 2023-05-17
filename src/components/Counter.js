dbca5e6c313f9af41cbe65afb0f61dc40d96ff38
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-05-17 21:30:38 +0900

    0517 commit

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,35 +1,53 @@
 import React, { useState } from "react";
 
 export default function Counter() {
-  const [count, setCount] = useState(0);
-  const [show, setShow] = useState(true);
+  // const [count, setCount] = useState(0);
+  // const [show, setShow] = useState(true);
   const operators = ["+", "-", "*"];
-  const [operator, setOperator] = useState(operators[0]);
+  // const [operator, setOperator] = useState(operators[0]);
+
+  const [info, setInfo] = useState({
+    count: 0,
+    show: true,
+    operator: operators[0],
+  });
+
+  console.log(info);
 
   return (
     <div>
       <button
         onClick={() => {
           let result;
-          if (operator === "+") result = count + 1;
-          if (operator === "-") result = count - 1;
-          if (operator === "*") result = count * 1;
-          setCount(result);
+          if (info.operator === "+") result = info.count + 1;
+          if (info.operator === "-") result = info.count - 1;
+          if (info.operator === "*") result = info.count * 1;
+          // setCount(result);
+          setInfo({ ...info, count: result });
         }}
       >
-        {operator}1
+        {info.operator}1
       </button>
-      <button onClick={() => setShow(!show)}>Show and Hide</button>
+      <button
+        onClick={() => {
+          // info.show = !info.show;
+          // const newInfo = info;  얕은복사
+          setInfo({ ...info, show: !info.show });
+        }}
+      >
+        Show and Hide
+      </button>
       <button
         onClick={() => {
           const idx = Math.floor(Math.random() * operators.length);
-          setOperator(operators[idx]);
+          // setOperator(operators[idx]);
+          setInfo({ ...info, operator: operators[idx] });
         }}
       >
         Change Operator
       </button>
       <br />
-      {show && `Counter: ${count}`}
+      {info.show && `Counter: ${info.count}`}
     </div>
   );
 }
