43513a632289d980396093059b0aa2e658a4d385
Daily-manjoo <122348173+Daily-manjoo@users.noreply.github.com>
2023-05-19 21:23:18 +0900

    0519 commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import "./App.css";
-import Counter from "./components/Counter";
+// import Counter from "./components/Counter";
+import Counter2 from "./components/Counter2";
 
 function App() {
   return (
     <div className="app">
-      <Counter />
+      <Counter2 />
     </div>
   );
 }
